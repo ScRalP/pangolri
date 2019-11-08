b8a57d2ac03a45ceee7dec5a73ba5948cc5cd110
Robard Quentin <quentin.robard@etu.univ-lehavre.fr>
2019-11-08 12:55:15 +0100

    Ajout de la classe article

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -16,59 +16,8 @@ class Article
      */
     private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $libelle;
-
-    /**
-     * @ORM\Column(type="string", length=500, nullable=true)
-     */
-    private $description;
-
-    /**
-     * @ORM\Column(type="float")
-     */
-    private $prix;
-
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getLibelle(): ?string
-    {
-        return $this->libelle;
-    }
-
-    public function setLibelle(string $libelle): self
-    {
-        $this->libelle = $libelle;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(?string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getPrix(): ?float
-    {
-        return $this->prix;
-    }
-
-    public function setPrix(float $prix): self
-    {
-        $this->prix = $prix;
-
-        return $this;
     }
 }
