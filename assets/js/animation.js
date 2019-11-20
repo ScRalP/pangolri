c12ee5d1b95725dc86cc9efbde0ce518a7a6b26b
Robard Quentin <quentin.robard@etu.univ-lehavre.fr>
2019-11-20 09:41:45 +0100

    configuration de encore et du script animation.js

---
+++ assets/js/animation.js
@@ -0,0 +1,24 @@
+require('../css/style.css')
+
+const $ = require('jquery');
+
+$(document).ready(function(){
+    // Show or Hide The Sticky Footer Button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 200 ) {
+
+            $('.go-top').fadeIn(200);
+        } else {
+            $('.go-top').fadeOut(200);
+        }
+    });
+
+    // Animate the scroll to top
+    $('.go-top').click(function(e){
+
+        e.preventDefault();
+
+        $('html,body').animate({scrollTop: 0}, 300);
+
+    });
+});
